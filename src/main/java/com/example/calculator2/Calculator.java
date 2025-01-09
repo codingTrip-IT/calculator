7117ab50b7ee7738c3a4c77101127cb683f9af2e
mac <mac@macs-MacBook-Pro.local>
2025-01-09 11:41:24 +0900

    Refactor: Lv2 불필요한 필드 제거 🔨

--- src/main/java/com/example/calculator2/Calculator.java
+++ src/main/java/com/example/calculator2/Calculator.java
@@ -4,64 +4,22 @@ import java.util.*;
 
 public class Calculator {
     /* 필드 영역 */
-    private int num1;
-    private int num2;
-    private char operator;
-    private int result = 0;
     private ArrayList<Integer> resultList = new ArrayList<>();
 
     /* Getter 메서드 구현 */
-    public int getNum1() {
-        return num1;
-    }
-
-    public int getNum2() {
-        return num2;
-    }
-
-    public char getOperator() {
-        return operator;
-    }
-
-    public int getResult() {
-        return result;
-    }
-
     public ArrayList<Integer> getResultList(){
         return resultList;
     }
 
     /* Setter 메서드 구현 */
-    public int setNum1(int num1) {
-        this.num1 = num1;
-        return num1;
-    }
-
-    public int setNum2(int num2) {
-        this.num2 = num2;
-        return num2;
-    }
-
-    public char setOperator(char operator) {
-        this.operator = operator;
-        return operator;
-    }
-
-    public int setResult(int result) {
-        this.result = result;
-        return result;
-    }
-
     public ArrayList<Integer> setResultList(){
         this.resultList = resultList;
         return resultList;
     }
 
-    /* 생성자 영역 */
-
     /* 메서드 영역 */
     public int calculate(int num1, int num2, char operator) {
-
+        int result = 0;
         switch (operator) {
             case '+':
                 result = num1 + num2;
@@ -73,10 +31,6 @@ public class Calculator {
                 result = num1 * num2;
                 break;
             case '/':
-//                if (num2 == 0) {
-//                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-//                    break;
-//                }
                 result = num1 / num2;
                 break;
             default:
